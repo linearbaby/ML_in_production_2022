de370072732804c62f6152adf3666377b2a5fe55
ArtemPushPop <artemiygotovtsev@gmail.com>
2022-05-02 16:47:09 +0300

    added training pipeline. First working version of project

--- project/pipeline.py
+++ project/pipeline.py
@@ -4,20 +4,25 @@ import hydra
 import pandas as pd
 from omegaconf import DictConfig, OmegaConf
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from hydra.utils import get_original_cwd
+import logging
+
+log = logging.getLogger(__name__)
 
 
-def train(model, X, y):
-    epochs = 20
-    for epoch in range(epochs):
-        model.fit(X, y)
-
-        
 @hydra.main(config_path="configs", config_name="main_conf")
 def main(cfg: OmegaConf):
-    ds = get_dataset()
-    X_train, X_test, y_train, y_test = preprocess_pipeline(ds)
+    # instantiate root_path for propriate file management
+    log.debug('instantiate root_path for propriate file management')
+    cfg.core.root_path = get_original_cwd()
+
+    ds = get_dataset(config=cfg.data)
+    X_train, X_test, y_train, y_test = preprocess_pipeline(cfg.pipeline, ds)
     model = LogisticRegression()
-    result = model.fit(X_train, y_train)
+    model.fit(X_train, y_train)
+    print(accuracy_score(model.predict(X_test), y_test))
+    pass
 
-
-
+if __name__ == "__main__":
+    main()
